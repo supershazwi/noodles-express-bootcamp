9d1a6a9e604d695c9363ab66abe43ae4eded0a31
michelle-mok <michellemokhuihui@gmail.com>
2021-07-02 20:45:56 +0800

    first commit noodles ejs base

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js'; 
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+const PORT = 3004;
+
+const whenIncomingRequest = (req, res) => {
+  console.log('request  came in');
+
+  read('data.json', (data, err) => {
+    if (err) {
+      console.log('read error', err);
+    }
+
+    const { recipes } = data; 
+    res.render('index', { recipes });
+  });
+};
+
+const whenIncomingRequestIndex = (req, res) => {
+  console.log('request came in ');
+
+  read('data.json', (data, error) => {
+    if (error) {
+      console.log('read error', error);
+    }
+
+    const { index } = req.params;
+
+    const recipe = data.recipes[index];
+    console.log(recipe);
+
+    res.render('recipe', { recipe });
+  });
+};
+
+app.get('/', whenIncomingRequest);
+app.get('/recipes/:index', whenIncomingRequestIndex);
+app.listen(PORT);
